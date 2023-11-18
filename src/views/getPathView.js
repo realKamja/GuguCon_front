04b9c4af20fcefa179f172ea788dddfa35bf0b14
dandamdandam <glue0440@gmail.com>
2023-11-19 06:39:17 +0900

    jfeoiwfjaiwoghvapugh

--- src/views/getPathView.js
+++ src/views/getPathView.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import '../styles/getpath.scss';
 function GetPathView() {
     const [path, setPath] = useState([{latitude: 37.4526437, longitude: 126.49236}]);
@@ -13,20 +15,17 @@ function GetPathView() {
         let calcDistance = (p) => {
             return Math.sqrt(Math.pow(p.latitude - lat, 2) + Math.pow(p.longitude - lng, 2));
         }
-        // TODO 군집 대표 좌표 리스트 서버에서 받아오기
-        var coordinates = [
-            {latitude: 37.4526437, longitude: 126.49236},
-            {latitude: 37.4768068, longitude: 126.4847975},
-            {latitude: 37.4988237, longitude: 126.4960839},
-        ];
-        
-        // 거리를 기준으로 정렬
-        coordinates.sort((a, b) => calcDistance(a)-calcDistance(b));
-        
-        setPath([
-            coordinates[0],
-            coordinates[1],
-        ]);
+        // 군집 대표 좌표 리스트 서버에서 받아오기
+        axios.get('/api/report/location/union')
+          .then(res => {
+            // 거리를 기준으로 정렬
+            res.sort((a, b) => calcDistance(a)-calcDistance(b));
+            
+            setPath([
+                res[0],
+                res[1],
+            ]);
+          }).catch(error => {console.log(error);})
     }, [])
 
     return (
